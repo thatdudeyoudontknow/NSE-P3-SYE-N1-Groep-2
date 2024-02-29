92542a4085b019c377b23980940d7c9681850883
tom <t.a.stuurman@st.hanze.nl>
2024-02-29 13:42:59 +0100

    temp save

--- SYE2 Security praktijk/Security praktijk/week 2/test.py
+++ SYE2 Security praktijk/Security praktijk/week 2/test.py
@@ -10,8 +10,7 @@ import ipaddress
 import socket
 import psutil
 from pythonping import ping
-from scapy.all import srp
-from scapy.layers.l2 import ARP, Ether
+from scapy.all import srp, ARP,Ether
 import threading
 import subprocess
 import nmap
@@ -19,9 +18,8 @@ from tqdm import tqdm
 import time
 
 # Open the file objects
-# Open the file objects
-live_hosts_file_global = open("live_hosts.txt", "a", encoding='utf-8')
-unreachable_hosts_file = open("unreachable_hosts.txt", "a", encoding='utf-8')
+live_hosts_file = open("live_hosts.txt", "a")
+unreachable_hosts_file = open("unreachable_hosts.txt", "a")
 
 # Automatic Private IP Addressing (APIPA) prefix
 APIPA_PREFIX = "169.254"
@@ -53,62 +51,38 @@ def get_network_adapters():
 
     return available_networks
 
+def long_running_function():
+    for i in tqdm(range(100)):
+        time.sleep(0.01)
+
 
 def scan_port(host, port, open_ports):
-    """
-    Scans a specific port on a host. If the port is open, it adds the port and 
-    the service running on it to the open_ports list.
-    Parameters:
-    host (str): The host to scan.
-    port (int): The port to scan.
-    open_ports (list): A list to store the open ports and their services.
-    Returns:
-    None
-    """
     try:
-        # Maak een TCP socket
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Maak een TCP socket
         s.settimeout(1)  # Stel de time-out in op 1 seconde
-        # Probeert verbinding te maken met het opgegeven adres en poort
-        result = s.connect_ex((host, port))
+        result = s.connect_ex((host, port))  # Probeert verbinding te maken met het opgegeven adres en poort
         s.close()  # Sluit de socket
-        # Als de poort open is (0 betekent succesvolle verbinding)
-        if result == 0:
+        if result == 0:  # Als de poort open is (0 betekent succesvolle verbinding)
             nm = nmap.PortScanner()
             nm.scan(host, str(port))
             service = nm[host]['tcp'][port]['name']
-            # Voeg de open poort toe aan de lijst met open poorten
-            open_ports.append((port, service))
-    except socket.error as e:  # Vang eventuele fouten op
+            open_ports.append((port, service))  # Voeg de open poort toe aan de lijst
+    except Exception as e:  # Vang eventuele fouten op
         print(f"Fout bij het scannen van poort {port}: {e}")  # Druk de fout af
 
 
-def scan_ports(live_hosts_file):
-    """
-    Scans a range of ports on a list of hosts. 
-    The hosts are read from a file named 'live_hosts.txt'.
-    For each host, it creates a new thread for each port to be scanned. 
-    It then waits for all threads to complete.
-    After all ports have been scanned, 
-    it prints the host and any open ports found.
-
-    Parameters:
-    None
-
-    Returns:
-    None
-    """
-    #  Lijst van IP-adressen om te scannen
-    #  gets all the live hosts from live_hosts_file
-    #  and stores them in a list
-
-    live_hosts = live_hosts_file.readlines()
+def scan_ports():
+    # Lijst van IP-adressen om te scannen
+    global live_hosts_file
+    #gets all the live hosts from live_hosts_file and stores them in a list
+    with open('live_hosts.txt', 'r') as live_hosts_file:
+        live_hosts = live_hosts_file.readlines()
     target_hosts = [host.split()[1] for host in live_hosts]
+    
     min_port = 1  # Minimale poort
     max_port = 65535  # Maximale poort
 
-    print(f"Scannen van poorten {min_port} "
-          f"t/m {max_port} op de volgende IP-adressen: {target_hosts}")
+    print(f"Scannen van poorten {min_port} t/m {max_port} op de volgende IP-adressen: {target_hosts}")
 
     # Loop over alle IP-adressen en scan de poorten
     for target_host in target_hosts:
@@ -117,32 +91,45 @@ def scan_ports(live_hosts_file):
         # Multithreading: maak een thread voor elke poort en start deze
         threads = []
         for port in range(min_port, max_port + 1):
-            #  Maak een thread voor het scannen van de poort
-            thread = threading.Thread(target=scan_port, args=(
-                target_host, port, open_ports))
-            # Voeg de thread toe aan de lijst met threads
-            threads.append(thread)
+            thread = threading.Thread(target=scan_port, args=(target_host, port, open_ports))  # Maak een thread voor het scannen van de poort
+            threads.append(thread)  # Voeg de thread toe aan de lijst met threads
             thread.start()  # Start de thread
 
         # Wacht tot alle threads zijn voltooid
         for thread in threads:
             thread.join()  # Wacht op de voltooiing van elke thread
 
-        open_ports_str = ", ".join([f"{port} ({service})"
-                                    for port, service in open_ports])
+        open_ports_str = ", ".join([f"{port} ({service})" for port, service in open_ports])
         print(f"IP: {target_host}, OpenPoort(en): {open_ports_str}")
 
 
+# def arp_a(ip_address):
+#     try:
+#         # Voer de 'arp -a' opdracht uit en decodeer de uitvoer naar een leesbare string
+#         arp_output = subprocess.check_output(['arp', '-a', ip_address]).decode('utf-8')
+#         return arp_output  # Retourneer de uitvoer van 'arp -a'
+#     except subprocess.CalledProcessError:
+#         return None  # Retourneer None als er een fout optreedt tijdens het uitvoeren van de opdracht
+
+# def find_MAC():
+#     # Lijst van IP-adressen om te scannen
+#     global live_hosts_file
+#     #gets all the live hosts from live_hosts_file and stores them in a list
+#     with open('live_hosts.txt', 'r') as live_hosts_file:
+#         live_hosts = live_hosts_file.readlines()
+#     print (f"Scanning MAC address for {live_hosts}")
+#     target_hosts = [host.split()[1] for host in live_hosts]
+#     print (f"Scanning MAC address for {target_hosts}")
+#     for ip_address in target_hosts:
+#         print (f"Scanning MAC address for {ip_address}")
+#         arp_result = arp_a(ip_address)  # Voer 'arp -a' uit voor het huidige IP-adres
+#         if arp_result:
+#             print(f"ARP result for {ip_address}:")
+#             print(arp_result)  # Print de ARP-resultaten als er resultaten zijn
+#         else:
+#             print(f"No ARP result found for {ip_address}")  # Print een melding als er geen resultaten zijn gevonden
+
 def get_hostname(ip_address):
-    """
-    Resolves the hostname from the given IP address.
-
-    Parameters:
-    ip_address (str): The IP address to resolve.
-
-    Returns:
-    str: The hostname if it can be resolved, otherwise returns "Unable to resolve hostname".
-    """
     try:
         # Resolve the IP address to hostname
         hostname, _, _ = socket.gethostbyaddr(ip_address)
@@ -151,63 +138,29 @@ def get_hostname(ip_address):
         return "Unable to resolve hostname"
 
 
-def scan_live_ips(cidr):
+def scan_live_ips(target_ip):
+    # Maak een ARP-pakket om uit te zenden
+    arp = ARP(pdst=target_ip)
+    # Maak een Ethernet-frame
+    ether = Ether(dst="ff:ff:ff:ff:ff:ff")
+    # Combineer het Ethernet-frame en het ARP-pakket
+    packet = ether / arp
+    # Verzend het pakket en ontvang antwoorden
+    result = srp(packet, timeout=3, verbose=0)[0]
+    # Lijst om live IP-adressen op te slaan
+    live_ips = []
+    # Loop door de ontvangen antwoorden
+    for sent, received in result:
+        # Voeg het IP-adres toe aan de lijst met live IPs
+        live_ips.append(received.psrc)
+        print(received.psrc)
+        ip=received.psrc
+        mac_address = get_mac_address(str(ip))
+        os_type = scan_ip(str(ip))
+        hostname = get_hostname(str(ip))
+        live_hosts_file.write(f"IP: {ip} MAC: {mac_address} OS: {os_type} Hostname: {hostname}  is live.\n")
 
-    """
-    Ping all IP addresses in a given CIDR network and return a list
-    of live hosts.
-    Args:
-        cidr (str): The CIDR notation representing the network.
-    Returns:
-        list: A list of live host IP addresses.
-    """
-    # Verkrijg basis-IP-adres en aantal hostbits van CIDR-notatie
-    ip_network = ipaddress.ip_network(cidr, strict=False)
-    base_ip = ip_network.network_address
-    subnet_mask = ip_network.netmask
-    host_bits = subnet_mask.max_prefixlen - ip_network.prefixlen
-
-    # Bepaal het aantal IP-adressen in het subnetwerk
-    num_addresses = 2 ** host_bits
-
-    # Define the file objects
-    global live_hosts_file_global, unreachable_hosts_file
-    live_hosts_file_global = open("live_hosts.txt", "a", encoding='utf-8')
-    unreachable_hosts_file = open("unreachable_hosts.txt", "a", encoding='utf-8')
-    print (arp_output = subprocess.check_output(
-            ['arp', '-a', ip_address]).decode('utf-8'))
-    # Ping each IP address in the subnet
-    live_hosts = []
-    for i in tqdm(range(num_addresses)):
-        # if i reaches 50, stop the loop
-        if i == 50:
-            break
-        ip = base_ip + i
-        response = ping(str(ip), count=1)
-        if response.success():
-            mac_address = get_mac_address(str(ip))
-            #  call scan_ip function
-            os_type = scan_ip(str(ip))
-            hostname = get_hostname(str(ip))
-            live_hosts_file_global.write(f"IP: {ip} "
-                                  f"MAC: {mac_address} "
-                                  f"OS: {os_type} "
-                                  f"Hostname: {hostname} "
-                                  f"is live.\n")
-            print(f"\nHit on\n"
-                  f"IP: {ip} \n"
-                  f"MAC: {mac_address} \n"
-                  f"OS: {os_type} \n"
-                  f"Hostname: {hostname} \n")
-            live_hosts.append((ip, mac_address))
-        else:
-            # print(f"{ip} is not reachable.")
-            unreachable_hosts_file.write(f"{ip} is not reachable.\n")
-
-    # Close the file objects
-    live_hosts_file_global.close()
-    unreachable_hosts_file.close()
-    return live_hosts
+    return live_ips
 
 
 def clear_terminal():
@@ -236,9 +189,7 @@ def ping_cidr_network(cidr):
     num_addresses = 2 ** host_bits
 
     # Define the file objects
-    global live_hosts_file_global, unreachable_hosts_file
-    live_hosts_file_global = open("live_hosts.txt", "a", encoding='utf-8')
-    unreachable_hosts_file = open("unreachable_hosts.txt", "a", encoding='utf-8')
+    global live_hosts_file, unreachable_hosts_file
 
     # Ping each IP address in the subnet
     live_hosts = []
@@ -250,29 +201,22 @@ def ping_cidr_network(cidr):
         response = ping(str(ip), count=1)
         if response.success():
             mac_address = get_mac_address(str(ip))
-            #  call scan_ip function
+            #call scan_ip function 
             os_type = scan_ip(str(ip))
             hostname = get_hostname(str(ip))
-            live_hosts_file_global.write(f"IP: {ip} "
-                                  f"MAC: {mac_address} "
-                                  f"OS: {os_type} "
-                                  f"Hostname: {hostname} "
-                                  f"is live.\n")
-            print(f"\nHit on\n"
-                  f"IP: {ip} \n"
-                  f"MAC: {mac_address} \n"
-                  f"OS: {os_type} \n"
-                  f"Hostname: {hostname} \n")
+            live_hosts_file.write(f"IP: {ip} MAC: {mac_address} OS: {os_type} Hostname: {hostname}  is live.\n")
+            print(f"\nHit on\nIP: {ip} \nMAC: {mac_address} \nOS: {os_type} \nHostname: {hostname}  \n")
             live_hosts.append((ip, mac_address))
+
+
         else:
             # print(f"{ip} is not reachable.")
             unreachable_hosts_file.write(f"{ip} is not reachable.\n")
 
     # Close the file objects
-    live_hosts_file_global.close()
+    live_hosts_file.close()
     unreachable_hosts_file.close()
     return live_hosts
-
 
 def scan_ip(ip_address):
     scanner = nmap.PortScanner()
@@ -288,20 +232,20 @@ def scan_ip(ip_address):
 def get_mac_address(ip_address):
 
     try:
-        #  Voer de 'arp -a' opdracht uit en
-        #  decodeer de uitvoer naar een leesbare string
-        arp_output = subprocess.check_output(
-            ['arp', '-a', ip_address]).decode('utf-8')
-        #  return the output of 'arp -a' but only the mac address
-        #  if the arp output is empty, return unknown
+        # Voer de 'arp -a' opdracht uit en decodeer de uitvoer naar een leesbare string
+        arp_output = subprocess.check_output(['arp', '-a', ip_address]).decode('utf-8')
+        #return the output of 'arp -a' but only the mac address
+        #if the arp output is empty, return unknown
         if not arp_output:
             return "Unknown"
         else:
 
             return arp_output.split()[10]
     except subprocess.CalledProcessError:
-        print(f"No MAC address found for {ip_address}")
+        print (f"No MAC address found for {ip_address}")
         return None
+
+
 
 
 # Voorbeeldgebruik:
@@ -333,7 +277,6 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-
 def main():
     """
     This function is the entry point of the network scanner program.
@@ -341,7 +284,9 @@ def main():
     get the IP range from ipconfig.
     """
     clear_terminal()
-    print(f"{bcolors.WARNING}Warning: test{bcolors.ENDC}")
+    
+    
+    print(f"{bcolors.WARNING}Warning: No active frommets remain. Continue?{bcolors.ENDC}")
     print("1. Enter IP range manually")
     print("2. Get IP range from ipconfig")
     choice = input("Enter your choice (1 or 2): ")
@@ -361,21 +306,19 @@ def main():
         choice = input("Enter your choice (1 or 2): ")
         print(f"IP address of {ip_address}")
         print(ip_address)
-
-        if choice == '2' and ip_address:
+        
+        if choice== '2' and ip_address:
             abe(ip_address)
-            port_scan = input(
-                "Do you want to scan the live hosts for open ports? (y/n): ")
+            port_scan = input("Do you want to scan the live hosts for open ports? (y/n): ")
             if port_scan == 'y':
-                scan_ports(live_hosts_file_global)
+                scan_ports()
         if choice == '1':
             scan_live_ips(ip_address)
-            port_scan = input(
-                "Do you want to scan the live hosts for open ports? (y/n): ")
+            port_scan = input("Do you want to scan the live hosts for open ports? (y/n): ")
             if port_scan == 'y':
-                scan_ports(live_hosts_file_global)
+                scan_ports()
             else:
-                #  exit the program
+                #exit the program
                 exit()
         if not ip_address:
             print(f"No IP found for {selected_adapter}")
